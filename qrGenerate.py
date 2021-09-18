47c89be7f7d34ec519d5f530d9483262db0174cf
yourwallet-yowa <90962562+yourwallet-yowa@users.noreply.github.com>
2021-09-18 13:17:30 +0300

    Update qrGenerate.py

--- qrGenerate.py
+++ qrGenerate.py
@@ -1,8 +1,15 @@
+#For runninng qrGenerate.py
+#1- pip install qrcode
+#2- python qrGenerate.py value ex:  python qrGenerate.py 5.3
+#easy use:  open cmd ( terminal ) and enter " python qrGenerate.py 5.3 "
+
 import qrcode
 import sys
 
+walletAddress = "your wallet address"
+
 def generateQR(data):
-    img = qrcode.make(str(data))
+    img = qrcode.make(str(data)+":"+str(walletAddress))
     img.show()
 
 generateQR(sys.argv[1])
